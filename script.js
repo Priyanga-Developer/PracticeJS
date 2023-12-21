427a8031d9c52253c9da5da4170cbd892c9b4351
Priyanga <uthirapriyanga@gmail.com>
2023-12-21 21:19:02 +0530

    operators

---
+++ script.js
@@ -0,0 +1,37 @@
+// / Guess what answers you would get if you ran this in the
+// // Javascript Console in Google Chrome. Once you have an answer  to the questions then 
+// // check them by copying them and running it in the console yourself line by line 
+
+
+// //Evaluate the below:
+// 5 + "34"->534
+// 5 - "4"=1 (bcoz string is converted to number)
+// 10 % 5=0 (remainder)
+// 5 % 10=5
+// "Java" + "Script"-Java script
+// " " + " "
+// " " + 0 =' 0'
+// true + true =2
+// true + false =1
+// false + true=1
+// false - true=-1
+// 3 - 4 =-1
+// "Bob" - "bill"-NaN
+
+
+// //Evaluate the below comparisons:
+// 5 >= 1 false
+// 0 === 1 false
+// 4 <= 1 false
+// 1 != 1 false
+// "A" > "B" true
+// "B" < "C" false
+// "a" > "A"  You can see the values of each char at the ascii table below
+// a = 97 ACSI vlaues
+// A = 65 ACSI vlaues
+// "b" < "A"
+// true === false ->false
+// true != true false
+
+
+// // Make the string: "Hi There! It's "sunny" out" by using the + sign:
